96c9ebdf515b70a256a8705d930253591ea04da6
Trushil <trushil.budhia@gmail.com>
2018-09-06 20:10:53 +0800

    Changed random number folder to Rock, paper, scissor

--- src/actions/randomNumber.js
+++ src/actions/randomNumber.js
@@ -1,10 +1,27 @@
 
 export const GENERATE_RANDOMNUMBER = 'GENERATE_RANDOMNUMBER'
 export function generateRandomNumber() {
+  const RandomNumber = Math.floor(Math.random() * 3) + 1
+  let result = "-";
+	 
+  if (RandomNumber === 1) {
+    result = "Rock";
+  };
+
+  if (RandomNumber === 2) {
+    result = "Paper";
+  };
+  
+  if (RandomNumber === 3) {
+    result = "Scissor";
+  };
+
   return {
 	  type:  GENERATE_RANDOMNUMBER,
-	  number: Math.floor(Math.random() * 100) + 1
+	  number: result
   }
+
+  
 }
 
 
